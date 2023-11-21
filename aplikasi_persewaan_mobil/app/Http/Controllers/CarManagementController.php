8aa7b18e786b958caf5944cbeede5175edcec671
Fauzi Arya SA <57801172+fasa2297@users.noreply.github.com>
2023-11-21 18:32:26 +0700

    Typo fixed -> CarManagementController.php

--- aplikasi_persewaan_mobil/app/Http/Controllers/CarManagementController.php
+++ aplikasi_persewaan_mobil/app/Http/Controllers/CarManagementController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\CmentarManage;
+use App\Models\CarManagement;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Yajra\Datatables\Datatables;
